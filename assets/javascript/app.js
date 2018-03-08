0830d997d95cfc5c7ca21ddfa0e00a939981a1fa
Chris Jones <chrisjones0517@gmail.com>
2018-03-07 18:13:57 -0600

    styling additions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,60 +1,67 @@
+$(document).ready(function () {
 
-
-
-$(document).ready(() => {
-
-    let carArr = ['Corvette', 'Audi R8 V10', 'Lamborgini Centenario', 'Ferrari', 'Dodge Viper', 'Dodge Challenger Hellcat', 'Camaro Z-28', 'Alpha Romeo 4C', 'Bentley Mulsanne', 'Ford Mustang'];
+    let topics = ['Corvette', 'Audi R8 V10', 'Lamborgini Centenario', 'Ferrari', 'Dodge Viper', 'Dodge Challenger Hellcat', 'Camaro', 'Lexus', 'Bentley Mulsanne', 'Ford Mustang', 'Aston Martin', 'Tesla'];
     let car;
     let query;
     let limit = 10;
     let rating;
 
-
-    $.each(carArr, (index, item) => {
+    $.each(topics, function (index, item) {
         $('#carButtons').append(`<button>${item}</button>`);
     });
 
-    $('#addCar').on('click', (e) => {
+    $('#addCar').on('click', function (e) {
         e.preventDefault();
         car = $('#car-input').val();
+
         if (car !== '') {
             $('#carButtons').append(`<button class="added">${car}</button>`);
         }
     });
-    $('.added').click(() => {
-        console.log('new button clicked');
+
+    $('#clear').on('click', function () {
+        $('.added').remove();
     });
-    $('button').click((e) => {
+
+    $('#carButtons').on('click', 'button', function (e) {
         $('.gif').remove();
         query = e.target.textContent;
-        console.log(e);
+
         let queryURL = `https://api.giphy.com/v1/gifs/search?api_key=lpIfKCoXBupObInfL6H7Kx4yBATSBbXT&q=${query}&limit=${limit}&offset=0&rating=&lang=en`;
-        console.log(query);
-        console.log(queryURL);
+
         $.ajax({
             url: queryURL,
             method: 'GET'
-        }).then((res) => {
-            console.log(res.data);
-            $.each(res.data, (index, item) => {
-                let imgHt = item.images.original.height;
-                let imgWd = item.images.original.width;
-                let x = 125 / imgHt;
-                let newWd = x * imgWd;
-                $('#cars').append(`<div class="gif"><p>Rating: ${item.rating.toUpperCase()}</p><img src=${item.images.original.url} width=${newWd} height="125"></div>`);
-                $('.gif').css({'display':'inline-block', 'margin-right':'1rem'});
-                //console.log(item.images.preview_webp.url);
-            });
+        }).then(function (res) {
 
+            for (let i = 0; i < res.data.length; i++) {
+                let stillImage = res.data[i].images.original_still.url;
+                let animation = res.data[i].images.original.url;
+                $('#cars').append(`
+                <div class="gif">
+                    <p>Rating: ${res.data[i].rating.toUpperCase()}</p>
+                    <img src=${stillImage} data-animated=${animation} data-still=${stillImage} alt="car image" height="125">
+                </div>
+                `);
+                $('.gif').css({ 'display': 'inline-block', 'margin-right': '1rem' });
+            }
+        }).fail(function(xhr) {
+            alert('Sorry, there was a problem processing your request!');
+            console.log("Status: " + xhr.status);
+            console.dir(xhr);
         });
-
     });
 
+    $('#cars').on('click', 'img', function () {
+        let animated = $(this).attr('data-animated');
+        let still = $(this).attr('data-still');
 
-
-
-    // API key   lpIfKCoXBupObInfL6H7Kx4yBATSBbXT
-
-    // 
-
+        $(this).toggleClass('animated');
+        if (this.className === 'animated') {
+            $(this).attr('src', animated);
+        } else {
+            $(this).attr('src', still);
+        }
+    });
 });
+
